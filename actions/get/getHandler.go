dc5dbaec358004152912aa7679086dac1e1b6a29
mehdi <mehdi.shokohifar@gmail.com>
2023-12-31 02:40:42 +0330

    etc.Send added in action

--- actions/get/getHandler.go
+++ actions/get/getHandler.go
@@ -1,22 +1,22 @@
 package getArgs
 
 import (
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/mehdi-shokohi/inflow-fn/platform/std"
 )
-
-
 
 func GetHandler(c *fiber.Ctx)error{
 	body,err := std.GetBodyAs[User](c)
 	if err!=nil{
 		return c.JSON(fiber.ErrBadRequest)
 	}
+	
 	header,err := std.GetHeadersAs[map[string]interface{}](c)
 	if err!=nil{
 		return c.JSON(fiber.ErrBadRequest)
 	}
-	return c.JSON(struct{
+	return std.Send(c,fiber.StatusOK,struct{
 		Body *User
-		Header map[string]interface{}}{body,*header})
+		Header map[string]interface{}}{body,*header},nil)
 }
--- actions/get/user.go
+++ actions/get/user.go
@@ -1,7 +1,6 @@
 package getArgs
 
 type User struct {
-	
 	Name string `json:"user"`
 	Age  string `json:"age"`
 }
